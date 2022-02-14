330601f27c3da36c62ecbae8e01ea2e7bf5387cc
maxacaplan <maxacaplan@gmail.com>
2022-02-14 15:07:15 -0400

    added basic movment controls

--- src/classes/receiver.ts
+++ src/classes/receiver.ts
@@ -1,7 +1,13 @@
-import { point, receiver } from "../types";
+import { inputs, point, receiver, vector2d } from "../types";
+import { Vector2d } from "./vector2d";
 
 export class Receiver implements receiver {
   pos: point;
+  acc: number;
+  vel: vector2d;
+  termVel: number;
+  drag: number;
+
   radius: number;
 
   fillCol: string;
@@ -17,6 +23,11 @@ export class Receiver implements receiver {
     strokeWidth?: number
   ) {
     this.pos = { x, y };
+    this.acc = 10;
+    this.vel = new Vector2d();
+    this.termVel = 10;
+    this.drag = 0.25;
+
     this.radius = radius || 5;
 
     this.fillCol = fill || "#FFF";
@@ -34,5 +45,30 @@ export class Receiver implements receiver {
     ctx.strokeStyle = this.strokeCol;
     ctx.lineWidth = this.strokeWidth * unitRatio;
     ctx.stroke();
+  }
+
+  move(inputs: inputs, deltaTime: number) {
+    let move = new Vector2d();
+
+    if (inputs.up) move.y += 1;
+    if (inputs.down) move.y -= 1;
+    if (inputs.left) move.x -= 1;
+    if (inputs.right) move.x += 1;
+
+    move.normalize();
+
+    this.vel.add(move.mult(this.acc * deltaTime * 0.001));
+
+    // If velocity has magnitude apply drag
+    if (this.vel.mag() > 0.0001) {
+      this.vel.div(this.drag + 1);
+    } else {
+      this.vel.mult(0);
+    }
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
 }
